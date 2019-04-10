eb267ff507aa300fe7d045fe3cfc50090bedd15e
kushguptacse <kushguptacse@gmail.com>
2019-04-10 11:17:43 +0530

    resolve conflict

--- rabbitmq-mysql/src/main/java/sample/consumer/mysql/controller/RestControllerSample.java
+++ rabbitmq-mysql/src/main/java/sample/consumer/mysql/controller/RestControllerSample.java
@@ -3,10 +3,6 @@ package sample.consumer.mysql.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import sample.consumer.mysql.model.DataModel;
